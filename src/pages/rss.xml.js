65af28fc332f0568c1cf69fa641c25f024d7e655
seivarya <seivarya.he@gmail.com>
2026-03-20 15:05:46 +0530

    [fix]: ui bugs fixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,17 @@
 import rss from "@astrojs/rss";
 
-export function GET(context) {
+export async function GET(context) {
 	return rss({
-		title: "initial",
-		description: "",
-		site: context.site,
-		items: [],
+		title: "seivarya",
+		description: "test rss feed",
+		site: context.site || "http://localhost:4321",
+		items: [
+			{
+				title: "Test Post",
+				link: "/test",
+				pubDate: new Date(),
+				description: "hello world",
+			},
+		],
 	});
 }
